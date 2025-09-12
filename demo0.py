c219e9e41439a1c7e572fb462f1f71e7b462b6ac
JenkIke <jenkins.isaac2003@gmail.com>
2025-09-12 12:02:40 -0600

    added new files

---
+++ demo0.py
@@ -0,0 +1,11 @@
+name = str(input("What is your name? "))
+age = int(input("How old are you? "))
+y = 4.5
+inclass = True
+playing = False
+
+print(name)
+print(age)
+print(y)
+print(inclass)
+print(playing)
