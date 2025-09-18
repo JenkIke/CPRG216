6d764e77d6774add2be19a84275b487c5d260a88
JenkIke <jenkins.isaac2003@gmail.com>
2025-09-18 15:41:58 -0600

    added new files

---
+++ class_excercise1.py
@@ -0,0 +1,24 @@
+# This is the third in class demo #
+'''
+we will be doing stuff, or so Hani claims
+this could be multiple lines
+'''
+
+'''
+Review
+
+4 primitive types in python, string, boolean, int, float
+'''
+
+w = "word"
+x = 1
+y = 2.5
+z = True
+
+
+print(w, type(w))
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+print(type(print))
+print(type(print()))
