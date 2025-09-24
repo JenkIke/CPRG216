6380fd05e9a2acf74f96dd21fe0019a29b705a44
JenkIke <jenkins.isaac2003@gmail.com>
2025-09-24 09:53:58 -0600

    saving work

--- class_excercise1.py
+++ class_excercise1.py
@@ -35,10 +35,47 @@ print(str(num_as_num)) # same thing, python casts with print statement
 '''
 
 # Using the input function, input function always returns a string (text)
-
+'''
 year_of_birth = int(input("Please enter your year of birth\n"))
 Month_of_birth = int(input("Please enter your month of birth (1-12)\n"))
 if Month_of_birth > 9:
     print("Your age is", 2024 - year_of_birth)
 else:
     print("Your age is", 2025 - year_of_birth)
+'''
+
+# print function with seperators
+
+print("Hello", "world", "this", "is", "a", "test", sep="***", end="\n")
+
+# \n is a new line, \t is a tab
+
+print("Hello\nworld\nthis\nis\na\ntest")
+print("Hello\tworld\tthis\tis\ta\ttest")
+
+# these are called escape characters, they allow you to do special things in strings with a backslash
+# \n, \t, \', \", \\
+print('I\'m a student')
+print("He said \"Hello\" to me")
+print("This is a backslash: \\")
+
+# precedence rules
+
+expression = 3 + 4 * 5 - 6 / 2
+print(expression)
+#BEDMAS
+
+complex_expression = 4/2*3
+print(complex_expression)
+complex_expression2 = 4/(2*3)
+# in this case you must wor left to right for precedence
+
+#More about assignment statements
+x = 3
+x = x + 2
+print(x)
+# x = 5
+
+# augmented assignment
+x += 2 # same as x = x + 2
+print(x)
