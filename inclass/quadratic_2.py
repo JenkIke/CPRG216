20b224dcd37a2e513331ad4378ee4d38e32a6526
JenkIke <jenkins.isaac2003@gmail.com>
2025-10-03 11:57:36 -0600

    in class

---
+++ inclass/quadratic_2.py
@@ -0,0 +1,6 @@
+print("Welcome to the Quadratic Equation Solver!")
+print("The standard form of a quadratic equation is: ax^2 + bx + c = 0")
+a = float(input("Enter coefficient a: \n"))
+b = float(input("Enter coefficient b: "))
+c = float(input(""))
+
---
+++ inclass/week5.py
@@ -0,0 +1,8 @@
+#understanding the while loop
+i = 1558
+n = 1
+
+while i > 0:
+    n *= i
+    i -= 1
+print(n)
